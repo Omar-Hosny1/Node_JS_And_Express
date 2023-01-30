6e32f24c1e60fd55d24808fad4a327769d84d263
Omar-Hosny1 <honsyomar022@gmail.com>
2023-01-30 14:21:08 +0200

    post method added

--- app.js
+++ app.js
@@ -1,16 +1,23 @@
 const express = require("express");
+const bodyParser = require("body-parser");
 
 const app = express();
 
+// it not parses the all data types but it can user for forms
+app.use(bodyParser.urlencoded({ extended: false })); // middleware that parse the body for the incoming requests
+
 app.use("/add-product", (req, res, next) => {
-  // add a middleware function
-  console.log("In another middle ware");
-  return res.send("<h1>Add Product Now</h1>");
+  return res.send(
+    "<form action='/product' method='POST'><input type='text' name='title' /><button type='submit'>Add Product</button></form>"
+  );
+});
+
+app.post("/product", (req, res, next) => {
+  console.log(req.body);
+  res.redirect("/");
 });
 
 app.use("/", (req, res, next) => {
-  // add a middleware function
-  console.log("In another middle ware");
   return res.send("<h1>Hello from express</h1>");
 });
 // the middleware will excute when every incoming request
